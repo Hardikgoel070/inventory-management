7652c1739b772286c0c32ea737ba4ef134657aa9
Hardikgoel070 <b22bb048@iitj.ac.in>
2025-02-13 02:35:17 +0530

    added images

---
+++ client/next.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "s3-iinventorymanagement.s3.us-east-1.amazonaws.com",
+          port: "",
+          pathname: "/**",
+        },
+      ],
+    },
+  };
+  
+  export default nextConfig;
+  
